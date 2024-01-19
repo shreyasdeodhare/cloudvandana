da353ca75a121c1f1ae74c912eaea1139c4b8179
Shreyas Deodhare <67466611+shreyasdeodhare@users.noreply.github.com>
2024-01-19 17:46:43 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.* ;
+class Solution{
+public static boolean pali(String s){ 
+     StringBuffer sb=new StringBuffer(s); 
+     if(s.equals(sb.reverse().toString())) return true; 
+
+   return false;
+
+
+
+}
+  public static void main(String[] args){ 
+     String s="Ramar" ;
+    System.out.println("IS a Paliindrome" +pali(s); 
+}
+}
