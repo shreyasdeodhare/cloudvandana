6f6bdd782458ff53f3dd33141e7aef1d9b41ee41
Shreyas Deodhare <67466611+shreyasdeodhare@users.noreply.github.com>
2024-01-19 18:00:42 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,7 +1,9 @@
 import java.util.*;
 import java.lang.* ;
 class Solution{
-public static boolean pali(String s){ 
+public static boolean pali(String s){
+
+     s=s.toLowerCase(); 
 
      StringBuffer sb=new StringBuffer(s); 
      if(s.equals(sb.reverse().toString())) return true; 
